29bc6b964b2e993fb6482902d2b3911dbe0a31e1
Aakarsh Nair <aakarsh.nair@gmail.com>
2024-09-17 19:09:51 +0000

    adding load bids path.

--- epocher/dataset.py
+++ epocher/dataset.py
@@ -1,6 +1,15 @@
 
+
+import numpy as np
+import mne
+from mne_bids import BIDSPath # Import the BIDSPath class
+
+# Create a BIDSPath object
+
+
 DATASET_ROOT="/content/drive/MyDrive/TUE-SUMMER-2024/ulm-meg/"
 
-def load_bids_path():
-    pass
+def load_bids_path(root=DATASET_ROOT, subject="sub-01", datatype="meg", session="0"):
+    bids_path = BIDSPath(root=DATASET_ROOT+"/"+subject, session=session, datatype=datatype)
+    return bids_path
 
