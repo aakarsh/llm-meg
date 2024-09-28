62a9dea45c59ebcda0b924338d2525ec9e8b3dc9
Aakarsh Nair <aakarsh.nair@gmail.com>
2024-09-28 18:52:46 +0000

    adding mroe comparisons.

--- epocher/plots.py
+++ epocher/plots.py
@@ -14,22 +14,26 @@ from sklearn.manifold import TSNE
 import pandas as pd
 import json
 
-
+from .env import *
 
 def load_similarity_matrix(subject_id, task_id):
     word_index_file = f'{OUTPUT_DIR}/subject_{subject_id}_task_{task_id}_word_index.json'
     similarity_matrix_file = f'{OUTPUT_DIR}/subject_{subject_id}_task_{task_id}_similarity_matrix.npy'
 
     word_index = None
-    with open(file_path, 'r') as infile:
+    with open(word_index_file, 'r') as infile:
          word_index = json.load(infile)
 
+    similarity_matrix = np.load(similarity_matrix_file)
+
+    return word_index, similarity_matrix
     
 
-def plot_saved_similarity_matrix(subject_id, task_id):
-    file_path = f"./images/subject_id_{subject_id}_task_id_{task_id}_similarity_matrix.png") 
-    
-    plot_similarity_matrix(word_index, similarity_matrix, file_path)
+def plot_saved_similarity_matrix(subject_id=None, task_id=None):
+    file_path = f"./images/subject_id_{subject_id}_task_id_{task_id}_similarity_matrix.png" 
+  
+    word_index, similarity_matrix = load_similarity_matrix(subject_id, task_id)
+    plot_similarity_matrix(word_index, similarity_matrix, file_path=file_path)
 
 def plot_similarity_matrix(word_index, similarity_matrix, h=160, w=128, file_path="./images/sim_words.png"):
     # Assuming 'similarity_matrix' is already computed
@@ -64,36 +68,36 @@ def plot_dendogram(word_index, similarity_matrix, h=80, w=64, file_path="./image
 
 
 def plot_tsne(word_index, similarity_matrix):
-	# Assuming 'similarity_matrix' is already computed and 'target_words' contains word labels
+    # Assuming 'similarity_matrix' is already computed and 'target_words' contains word labels
 
-	# Step 1: Compute the t-SNE Representation in 3D
-	tsne_model = TSNE(n_components=3, random_state=42, perplexity=5, n_iter=1000, metric="cosine")
-	tsne_results = tsne_model.fit_transform(similarity_matrix)
+    # Step 1: Compute the t-SNE Representation in 3D
+    tsne_model = TSNE(n_components=3, random_state=42, perplexity=5, n_iter=1000, metric="cosine")
+    tsne_results = tsne_model.fit_transform(similarity_matrix)
 
 
-	# Create a DataFrame with t-SNE results and words
-	tsne_df = pd.DataFrame(tsne_results, columns=['Component 1', 'Component 2', 'Component 3'])
-	tsne_df['Words'] = word_index 
+    # Create a DataFrame with t-SNE results and words
+    tsne_df = pd.DataFrame(tsne_results, columns=['Component 1', 'Component 2', 'Component 3'])
+    tsne_df['Words'] = word_index 
 
-	# Step 3: Create the 3D scatter plot using Plotly
-	fig = px.scatter_3d(tsne_df,
-						 x='Component 1',
-						 y='Component 2',
-						 z='Component 3',
-						 text='Words',
-						 title='3D t-SNE Visualization of Target Words',
-						 labels={'Component 1': 't-SNE Component 1',
-								 'Component 2': 't-SNE Component 2',
-								 'Component 3': 't-SNE Component 3'},
-						 template='plotly_white')
+    # Step 3: Create the 3D scatter plot using Plotly
+    fig = px.scatter_3d(tsne_df,
+                         x='Component 1',
+                         y='Component 2',
+                         z='Component 3',
+                         text='Words',
+                         title='3D t-SNE Visualization of Target Words',
+                         labels={'Component 1': 't-SNE Component 1',
+                                 'Component 2': 't-SNE Component 2',
+                                 'Component 3': 't-SNE Component 3'},
+                         template='plotly_white')
 
-	# Step 4: Customize the layout
-	fig.update_traces(marker=dict(size=5, opacity=0.8))
-	fig.update_layout(scene=dict(
-						xaxis_title='t-SNE Component 1',
-						yaxis_title='t-SNE Component 2',
-						zaxis_title='t-SNE Component 3'),
-					  margin=dict(l=0, r=0, b=0, t=40))
+    # Step 4: Customize the layout
+    fig.update_traces(marker=dict(size=5, opacity=0.8))
+    fig.update_layout(scene=dict(
+                        xaxis_title='t-SNE Component 1',
+                        yaxis_title='t-SNE Component 2',
+                        zaxis_title='t-SNE Component 3'),
+                      margin=dict(l=0, r=0, b=0, t=40))
 
-	# Step 5: Show the plot
-	fig.show()
+    # Step 5: Show the plot
+    fig.show()
--- scripts/rsa_gen.py
+++ scripts/rsa_gen.py
@@ -12,6 +12,7 @@ import epocher
 
 import epocher.rsa as rsa
 import epocher.dataset as D
+import epocher.plots as P
 
 def compute_rsa_similarity(subject1, subject2, task_id):
     """
@@ -54,13 +55,13 @@ def main():
 
     # Subcommand for generating similarity matrix for one subject
     generate_parser = subparsers.add_parser('generate', help='Generate similarity matrix for a subject')
-    generate_parser.add_argument('--subject', type=str, required=True, help='ID of the subject')
+    generate_parser.add_argument('--subject_id', type=str, required=True, help='ID of the subject')
     generate_parser.add_argument('--task_id', type=int, required=True, help='ID of the task')
 
     generate_parser = subparsers.add_parser('generate-all', help='Generate all similarity matrics for all tasks.')
 
     plot_rsa_tabe_parser = subparsers.add_parser('plot-rsa-table', help='Plot RSA Confusion Table for a sobject and task, use cached results')
-    plot_rsa_tabe_parser.add_argument('--subject', type=str, required=True, help='ID of the subject')
+    plot_rsa_tabe_parser.add_argument('--subject_id', type=str, required=True, help='ID of the subject')
     plot_rsa_tabe_parser.add_argument('--task_id', type=int, required=True, help='ID of the task')
 
     args = parser.parse_args()
@@ -71,11 +72,11 @@ def main():
         # Output the result
         print(f"RSA correlation between subject {args.subject1} and {args.subject2} for task {args.task_id}: {correlation:.2f}")
     elif args.command == 'generate':
-        compute_rsa_matrix(subject_id=args.subject, task_id=args.task_id)
+        compute_rsa_matrix(subject_id=args.subject_id, task_id=args.task_id)
     elif args.command == 'generate-all':
         compute_all_rsa_matrics()
     elif args.command == 'plot-rsa-table':
-        # plot rsa-table 
+        P.plot_saved_similarity_matrix(subject_id=args.subject_id, task_id=args.task_id)
     else:
         print("Please provide a valid command ('compare' or 'generate').")
 
