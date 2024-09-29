c2b7b1db59ac2e285a1bd0151b70241efc1f6a53
Aakarsh Nair <aakarsh.nair@gmail.com>
2024-09-29 18:08:15 +0000

    bert

--- epocher/llm_bert.py
+++ epocher/llm_bert.py
@@ -26,8 +26,10 @@ def get_index_stimulus_stories(word_index, task_id, use_cache=True):
     get_index_stimulus_stories - story_path
     """
     story_key = f'{task_stimuli[task_id]}.txt'
+    """
     if story_key in EMBEDDING_TASK_CACHE:
         return  EMBEDDING_TASK_CACHE[story_key]
+    """
 
     story_map = load_experiment_stories()
     story = story_map[story_key]
@@ -61,6 +63,7 @@ def get_index_stimulus_stories(word_index, task_id, use_cache=True):
         word_inputs = tokenizer(word_to_avg, return_tensors='pt', truncation=True, padding=True)
         decoded_word = tokenizer.decode(word_inputs['input_ids'][0])
         word_stem = decoded_word.split()[1]
+        print(f"word_stem : {word_stem}")
         if not word_stem in average_embeddings:
             print(f'skipping {word_to_avg}, {word_stem} not found ')
             continue
@@ -91,7 +94,7 @@ def compute_similarity_matrix(word_vectors):
 
 def create_rsa_matrix(words, task_id):
     word_vectors = get_index_stimulus_stories(words, task_id)
-    
+    print(f"len word vectors {len(word_vectors)}")
     # Normalize word vectors before computing cosine similarity
     normalized_vectors = normalize_vectors(word_vectors)
     
