22a5b3d135bcd2577c093c612f3fbc549b38ba06
Aakarsh Nair <aakarsh.nair@gmail.com>
2024-10-13 13:05:20 +0000

    adding env, and rsa experiemnts.

--- epocher/env.py
+++ epocher/env.py
@@ -2,7 +2,7 @@ MEG_MASC_ROOT="/content/drive/MyDrive/TUE-SUMMER-2024/ulm-meg/"
 MEG_MASC_STIMULI_DIRECTORY=MEG_MASC_ROOT+"/stimuli/text"
 
 OUTPUT_DIR="/content/llm-meg/output"
-IMAGES_DIR="/content/llm-meg/impages"
+IMAGES_DIR="/content/llm-meg/images"
 
 GLOVE_PATH="/content/drive/MyDrive/TUE-SUMMER-2024/ulm-meg/language-models/glove.840B.300d.txt"
 
